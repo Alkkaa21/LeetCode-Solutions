d26124a5c33d839c50c01be7ab55e02319ca5e4a
Alkkaa21 <alkaroy33@gmail.com>
2026-06-17 16:47:53 +0530

    Guess the number

---
+++ Binary search/guess number higher or lower.py
@@ -0,0 +1,23 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l,r=0,n
+        while l<=r:
+            m=(l+r)//2
+            if guess(m)==0:
+                return int(m)
+            elif guess(m)==-1:
+                 r=m-1
+            else:
+                l=m+1
+        
