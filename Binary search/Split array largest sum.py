1b127ef25f31912b77a3e952d5be48dabe526f5e
Alkkaa21 <alkaroy33@gmail.com>
2026-06-20 17:06:24 +0530

    Split array largest sum

---
+++ Binary search/Split array largest sum.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def splitArray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def cansplit(largest):
+            subarray=0
+            cursum=0
+            for n in nums:
+                cursum+=n
+                if cursum>largest:
+                    subarray+=1
+                    cursum=n
+            return subarray+1<=k
+        l,r=max(nums),sum(nums)
+        while l<=r:
+            m=(l+r)//2
+            if cansplit(m):
+                res=m
+                r=m-1
+            else:
+                l=m+1
+        return res
+        
