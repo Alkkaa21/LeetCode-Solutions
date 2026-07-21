a4c416f6909b30406420c885277741c5f8aa677d
Alkkaa21 <alkaroy33@gmail.com>
2026-07-21 23:46:32 +0530

    Longest consecutive sequence

---
+++ arrays/Longest consecutive sequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numset=set(nums)
+        res=0
+        
+        for n in numset:
+            if n-1  not in numset:
+                length=1
+              
+                current=n
+                while current+1 in numset:
+                    length+=1
+                    current+=1
+                res=max(length,res)
+
+        return res
+
