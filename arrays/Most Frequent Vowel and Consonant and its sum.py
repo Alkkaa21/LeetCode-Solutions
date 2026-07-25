0ab7db3e5787d6f00563d98b3ef2091eed9fc042
Alkkaa21 <alkaroy33@gmail.com>
2026-07-25 17:49:15 +0530

    Most Frequent Vowel and Consonant

---
+++ arrays/Most Frequent Vowel and Consonant and its sum.py
@@ -0,0 +1,14 @@
+vowels = {'a','e','i','o','u'}
+vow = {}
+cons = {}
+
+for ch in s:
+    if ch in vowels:
+        vow[ch] = 1 + vow.get(ch, 0)
+    else:
+        cons[ch] = 1 + cons.get(ch, 0)
+
+vowcount = max(vow.values()) if vow else 0
+conscount = max(cons.values()) if cons else 0
+
+return vowcount + conscount
