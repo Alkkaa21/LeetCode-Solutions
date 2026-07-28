95398faa4ccccef408cb51d469efcbdd51f6e625
Alkkaa21 <alkaroy33@gmail.com>
2026-07-28 23:39:02 +0530

    Small subset sum greater than all

--- Greedy/Small subset sumgreater than all.py
+++ Greedy/Small subset sumgreater than all.py
@@ -1,3 +1,7 @@
+#Given an array of non-negative integers, the task is to find the minimum number of elements such that their sum should be greater than the sum of the rest of the elements of the array.
+
+#Input: arr[] = [ 3 , 1 , 7, 1 ]
+#Output: 1
 class Solution:
     def minSubset(self, arr):
         # code here
