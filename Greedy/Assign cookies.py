0ceda4c544ebaca60cb644158d1ba6f70de96546
Alkkaa21 <alkaroy33@gmail.com>
2026-08-04 21:18:32 +0530

    Assign cookies

---
+++ Greedy/Assign cookies.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        l,r=0,0
+        count=0
+        while r<len(s) and l<len(g):
+            if s[r]>=g[l]:
+                l+=1
+                r+=1
+                count+=1
+            else:
+                r+=1
+        return count
+
