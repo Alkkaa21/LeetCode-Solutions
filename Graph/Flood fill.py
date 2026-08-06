19425fa31baff2c17aff9e6bd88e7ff79f48cc22
Alkkaa21 <alkaroy33@gmail.com>
2026-08-06 20:16:29 +0530

    flood fill

---
+++ Graph/Flood fill.py
@@ -0,0 +1,19 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        def dfs(r,c):
+            if r<0 or r>=rows or c<0 or c>=cols:
+                return
+            if image[r][c]==color:
+                return
+            if image[r][c]!=original:
+                return
+            image[r][c]=color
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        rows,cols=len(image),len(image[0])
+        original = image[sr][sc]
+        dfs(sr,sc)
+        return image
+    
