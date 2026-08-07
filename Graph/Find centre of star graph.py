8eae427b3e8280f158ffd4697565e2695d6651df
Alkkaa21 <alkaroy33@gmail.com>
2026-08-07 20:21:13 +0530

    find centre of star graph

---
+++ Graph/Find centre of star graph.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        a,b=edges[0]
+        c,d=edges[1]
+        if a==c or a==d:
+            return a
+        return b
