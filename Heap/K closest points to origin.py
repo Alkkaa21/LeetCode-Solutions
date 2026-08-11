fe30d6787d80e28c6f59dda28d51073088760cae
Alkkaa21 <alkaroy33@gmail.com>
2026-08-11 22:03:51 +0530

    K closest points to origin

---
+++ Heap/K closest points to origin.py
@@ -0,0 +1,13 @@
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap=[]
+        res=[]
+        for point in points:
+            x,y=point
+            distance=x*x+y*y
+            heapq.heappush(heap,(distance,point))
+        for _ in range(k):
+            distance,point=heapq.heappop(heap)
+            res.append(point)
+        return res
